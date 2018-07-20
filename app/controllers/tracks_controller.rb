91e9b9161e90a1c910bd4a893bd2fd04077f91a1
Caiden Robinson <caiden.robinson35@gmail.com>
2018-07-20 14:27:12 -0700

    WIP: Tracks#index returns a list of tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,10 +1,32 @@
+require "httparty"
+
 class TracksController < ApplicationController
   def index
-    base_url = Rails.application.credentials.beatroot[:base_url]
-    account_slug = Rails.application.credentials.beatroot[:account_slug]
-    auth_token = Rails.application.credentials.beatroot[:authentication_token]
+    url = "#{beatroot_api_url}/tracks"
+    header = { "Authorization" => "Token token=\"#{auth_token}\""}
+
+    response = HTTParty.get(url, headers: header)
+
+    @tracks = response.parsed_response["tracks"]
+    meta = response.parsed_response["meta"]
   end
 
   def show
+    #track_id = nil
+
+    url = "#{beatroot_api_url}/track/#{track_id}"
+  end
+
+  private
+
+  def beatroot_api_url
+    base_url = Rails.application.credentials.beatroot[:base_url]
+    account_slug = Rails.application.credentials.beatroot[:account_slug]
+
+    "#{base_url}/accounts/#{account_slug}"
+  end
+
+  def auth_token
+    Rails.application.credentials.beatroot[:authentication_token]
   end
 end
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,14 @@
 module TracksHelper
+  def track_to_li(track)
+    id = track["id"]
+    content_tag :li, class: "track", id: "track-#{id}" do
+      content_tag :p do
+        "#{track["title"]} - #{artist(track)}"
+      end
+    end
+  end
+
+  def artist(track)
+    track["artist"]["name"]
+  end
 end
